e9673d452cbbb47c0585d4478d255fe9e98debb6
LavishGulati <lavish00gulati@gmail.com>
2018-10-23 02:44:28 +0530

    Added solutions for lab 3C

---
+++ 3C/Solutions/1.sh
@@ -0,0 +1,12 @@
+<<c1
+Name: Lavish Gulati
+Roll No: 170101082
+Semester: III
+Shell program name: 1.sh
+c1
+
+echo "This is the first shell program. Please enter the username."
+read name
+echo "Username::"$name
+
+# End of the program
